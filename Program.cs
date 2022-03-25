77d6d239c82fd222e67ffd7a09f5477e1f2ba933
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-03-26 01:13:11 +0530

    Harmonic

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmonivValue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                double nth_Harmonic_Num = 0, reciprocal;
+                Console.WriteLine("Please enter a number to calculate Nth Harmonic value");
+                int n = Convert.ToInt32(Console.ReadLine());
+                if (n > 0)
+                {
+                    for (int i = 1; i <= n; i++)
+                    {
+                        reciprocal = (double)1 / i;
+                        nth_Harmonic_Num = nth_Harmonic_Num + reciprocal;
+                        Console.Write("1/" + i + " + ");
+                    }
+                    Console.WriteLine("\nNth Harmonic value is : " + nth_Harmonic_Num);
+                }
+                else
+                    Console.WriteLine("Please enter a number greater than zero");
+            }
+        }
+    }
+    
